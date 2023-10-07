713459a9d637c08eac41bba2b63385187ba6ad3f
jainam-b <jainambagrecha16@gmail.com>
2023-10-07 12:01:13 +0530

    Product model created

--- website/home/views.py
+++ website/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models  import Product
+from django.http import HttpResponse
+from math import ceil
 # Create your views here.
 
 def home(request):
-    return render(request, "home/homepage.html")
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"home/homepage.html", params)
